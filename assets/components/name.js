01ac10b0b20f650d7e2a4e42f1b02e15348fe34b
bendemonium <ridhib2422@gmail.com>
2025-11-24 21:17:32 -0600

    name

--- assets/components/name.js
+++ assets/components/name.js
@@ -2,8 +2,11 @@ class Name extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
         <div style="max-width: 9in; padding-bottom: 5px;">
-			<div style="font-size: 2.7em; font-weight: 500; font-family: 'Calibri Light', Helvetica, Arial, sans-serif;">Ridhi Bandaru</div>
-			<div style="font-size: 1.6em; font-weight: 400; font-family: 'Calibri Light', Helvetica, Arial, sans-serif; margin-top: 2px; margin-bottom: 25px;">/rɪdʱiː bəŋɖaːɾuː/ </div>
+      
+			<div style="font-size: 2.7em; font-weight: 500; font-family: 'Calibri Light', Helvetica, Arial, sans-serif;">
+        <img src="assets/name.svg" alt="Ridhi Bandaru" style="height:60px;">
+      </div>
+			<div style="font-size: 1.5em; font-weight: 400; font-family: 'Calibri Light', Helvetica, Arial, sans-serif; margin-top: -5px; margin-bottom: 15px;">/rɪdʱiː bəŋɖaːɾuː/ </div>
 			<hr style="border: 0.01px solid #53535359;"/>
 			<!-- <div style="font-size: 1em; font-weight: 50; font-family: 'Calibri Light', Calibri, Arial, sans-serif; margin-top: 2px;"><i>ri-dee ban-der-oo</i></div> -->
 		</div>
