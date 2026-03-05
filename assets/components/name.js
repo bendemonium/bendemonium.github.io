5cedf0418fb2a6e21d372d4902403834fc4fa311
bendemonium <ridhib2422@gmail.com>
2026-03-04 20:02:59 -0600

    minor change, dwbi

--- assets/components/name.js
+++ assets/components/name.js
@@ -6,7 +6,7 @@ class Name extends HTMLElement {
 			<div style="font-size: 2.7em; font-weight: 500; font-family: 'Calibri Light', Helvetica, Arial, sans-serif;">
         <img src="assets/name.svg" alt="Ridhi Bandaru" style="height:60px;">
       </div>
-			<div style="font-size: 1.3em; font-weight: 400; font-family: 'Calibri Light', Helvetica, Arial, sans-serif; margin-top: -5px; margin-bottom: 15px;">/rɪdʱiː bəŋɖaːɾuː/ </div>
+			<div style="font-size: 1.3em; font-weight: 400; font-family: 'Calibri Light', Helvetica, Arial, sans-serif; margin-top: -8px; margin-bottom: 15px;">/rɪdʱiː bəŋɖaːɾuː/ </div>
 			<hr style="border: 0.01px solid #53535359;"/>
 			<!-- <div style="font-size: 1em; font-weight: 50; font-family: 'Calibri Light', Calibri, Arial, sans-serif; margin-top: 2px;"><i>ri-dee ban-der-oo</i></div> -->
 		</div>
