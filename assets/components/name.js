953c62e1d174f3e6ffec48e1e69afe28210fa4bf
bendemonium <ridhib2422@gmail.com>
2026-03-06 22:30:52 -0600

    minor change, dwbi

--- assets/components/name.js
+++ assets/components/name.js
@@ -4,9 +4,9 @@ class Name extends HTMLElement {
         <div style="max-width: 9in; padding-bottom: 5px;">
       
 			<div style="font-size: 2.7em; font-weight: 500; font-family: 'Calibri Light', Helvetica, Arial, sans-serif;">
-        <img src="assets/name.svg" alt="Ridhi Bandaru" style="height:60px;"/>
+        <img src="assets/name.svg" alt="Ridhi Bandaru" style="height:50px;"/>
       </div>
-			<div style="font-size: 1.3em; font-weight: 400; font-family: 'Calibri Light', Helvetica, Arial, sans-serif; margin-top: -8px; margin-bottom: 15px;">/rɪdʱiː bəŋɖaːɾuː/ </div>
+			<div style="font-size: 1.2em; font-weight: 400; font-family: 'Calibri Light', Helvetica, Arial, sans-serif; margin-top: -10px; margin-bottom: 10px;">/rɪdʱiː bəŋɖaːɾuː/ </div>
 			<hr style="border: 0.01px solid #53535359;"/>
 			<!-- <div style="font-size: 1em; font-weight: 50; font-family: 'Calibri Light', Calibri, Arial, sans-serif; margin-top: 2px;"><i>ri-dee ban-der-oo</i></div> -->
 		</div>
--- assets/components/news.js
+++ assets/components/news.js
@@ -8,7 +8,7 @@ class News extends HTMLElement {
                     box-sizing: border-box;">
 			    <h3 style="margin-top: 0; font-size: 1em; font-weight: 300; font-family: 'Calibri Light', Helvetica, Calibri, Arial, sans-serif; color: #000000ff;">news / upcom!ng <hr style="border: 0.001px solid #3333332a"/> </h3>
 				<div style="max-height: 100px; overflow-y: auto; width: 100%; box-sizing: border-box; scrollbar-width: thin; scrollbar-color: #f1f1f1 #000000;">
-					<ul style="list-style: none; padding-left: 0; margin: 0; font-size: 0.9em; font-family: 'Calibri Light', Helvetica, Calibri, Arial, sans-serif;">
+					<ul style="list-style: none; padding-left: 0; margin: 0; font-size: 0.8em; font-family: 'Calibri Light', Helvetica, Calibri, Arial, sans-serif;">
 					<li style="display: flex; gap: 1.5rem; margin-bottom: 4px;">
 						<span style="min-width: 100px ; color: #44444483;">2025-Oct-23</span>
 						<span>
