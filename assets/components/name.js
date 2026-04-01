29fbb6269d8a89c4edbd5bd018ca5fd5a6be8fef
bendemonium <ridhib2422@gmail.com>
2026-03-31 19:43:38 -0500

    minor change, dwbi

--- assets/components/name.js
+++ assets/components/name.js
@@ -4,13 +4,23 @@ class Name extends HTMLElement {
         <div style="max-width: 9in; padding-bottom: 5px;">
       
 			<div style="font-size: 2.7em; font-weight: 500; font-family: 'Calibri Light', Helvetica, Arial, sans-serif;">
-        <img src="assets/name.svg" alt="Ridhi Bandaru" style="height:50px;"/>
+        <img id="name-image" src="assets/name_latin.svg" alt="Ridhi Bandaru" style="height:50px; cursor: pointer;"/>
       </div>
 			<div style="font-size: 1.2em; font-weight: 400; font-family: 'Calibri Light', Helvetica, Arial, sans-serif; margin-top: -10px; margin-bottom: 10px;">/rɪdʱiː bəŋɖaːɾuː/ </div>
 			<hr style="border: 0.01px solid #53535359;"/>
 			<!-- <div style="font-size: 1em; font-weight: 50; font-family: 'Calibri Light', Calibri, Arial, sans-serif; margin-top: 2px;"><i>ri-dee ban-der-oo</i></div> -->
 		</div>
     `;
+
+    // Add click event listener to toggle between SVG files
+    this.querySelector('#name-image').addEventListener('click', () => {
+      const img = this.querySelector('#name-image');
+      if (img.src.includes('name_latin.svg')) {
+        img.src = 'assets/name_brahmi.svg';
+      } else {
+        img.src = 'assets/name_latin.svg';
+      }
+    });
   }
 }
 
