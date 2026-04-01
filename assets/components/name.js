973175c7cfaf473a9a28431e01367845bc3f3696
bendemonium <ridhib2422@gmail.com>
2026-03-31 19:46:48 -0500

    minor change, dwbi

--- assets/components/name.js
+++ assets/components/name.js
@@ -4,7 +4,7 @@ class Name extends HTMLElement {
         <div style="max-width: 9in; padding-bottom: 5px;">
       
 			<div style="font-size: 2.7em; font-weight: 500; font-family: 'Calibri Light', Helvetica, Arial, sans-serif;">
-        <img id="name-image" src="assets/name_latin.svg" alt="Ridhi Bandaru" style="height:50px; cursor: pointer;"/>
+        <img id="name-image" src="assets/name_brahmi.svg" alt="Ridhi Bandaru" style="height:50px; cursor: pointer;"/>
       </div>
 			<div style="font-size: 1.2em; font-weight: 400; font-family: 'Calibri Light', Helvetica, Arial, sans-serif; margin-top: -10px; margin-bottom: 10px;">/rɪdʱiː bəŋɖaːɾuː/ </div>
 			<hr style="border: 0.01px solid #53535359;"/>
@@ -15,10 +15,10 @@ class Name extends HTMLElement {
     // Add click event listener to toggle between SVG files
     this.querySelector('#name-image').addEventListener('click', () => {
       const img = this.querySelector('#name-image');
-      if (img.src.includes('name_latin.svg')) {
-        img.src = 'assets/name_brahmi.svg';
-      } else {
+      if (img.src.includes('name_brahmi.svg')) {
         img.src = 'assets/name_latin.svg';
+      } else {
+        img.src = 'assets/name_brahmi.svg';
       }
     });
   }
