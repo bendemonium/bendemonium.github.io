ce292118f7079318f964c32dc4ea81ec0b350349
bendemonium <ridhib2422@gmail.com>
2026-04-11 18:17:15 -0500

    minor change, dwbi

--- assets/components/news.js
+++ assets/components/news.js
@@ -20,18 +20,18 @@ class News extends HTMLElement {
 
   parseDate(rawDate) {
     const monthMap = {
-      Jan: 1,
-      Feb: 2,
-      Mar: 3,
-      Apr: 4,
-      May: 5,
-      Jun: 6,
-      Jul: 7,
-      Aug: 8,
-      Sep: 9,
-      Oct: 10,
-      Nov: 11,
-      Dec: 12,
+      jan: 1,
+      feb: 2,
+      mar: 3,
+      apr: 4,
+      may: 5,
+      jun: 6,
+      jul: 7,
+      aug: 8,
+      sep: 9,
+      oct: 10,
+      nov: 11,
+      dec: 12,
     };
 
     const normalized = String(rawDate).trim();
